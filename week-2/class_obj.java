8adeef8c4872d0eee304313f06e51c901d76c7cc
Srivatsan0405 <srivatsanvenkatesan04@gmail.com>
2025-04-21 10:59:14 +0530

    v1

--- week-2/class_obj.java
+++ week-2/class_obj.java
@@ -1,4 +1,26 @@
 public class class_obj {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.start();
+        car.stop();
+    }
     
-    
+}
+abstract class Vehicle{
+
+    @SuppressWarnings("unused")
+    abstract void start();
+    @SuppressWarnings("unused")
+    abstract void stop();       
+}
+class Car extends Vehicle{
+    @Override
+
+    void start(){
+        System.out.println("Car started");
+    }
+    @Override
+    void stop(){
+        System.out.println("Car stopped");
+    }
 }
